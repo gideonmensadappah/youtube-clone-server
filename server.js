c1206342e849c26de6f7af1f1051b2dde5f3f360
Gideon Mensa <gideon123ewq@gmail.com>
2020-03-17 16:21:35 +0200

    Changed Server PORT

--- server.js
+++ server.js
@@ -1,24 +1,21 @@
-require('dotenv').config();     
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
-app.use(cors())
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-const Users =  require('./routes/Users');
-const Videos =  require('./routes/Videos');
+const Users = require("./routes/Users");
+const Videos = require("./routes/Videos");
 
-app.use('/users', Users);
-app.use('/videos', Videos);
+app.use("/users", Users);
+app.use("/videos", Videos);
 
+app.get("/", (req, res) => {
+  res.send("start");
+});
 
-app.get('/',(req, res)=> {
-        res.send('start')
-})
-
-
-
-app.listen(port, ()=>console.log(`on port ${port}...`));
+app.listen(port, () => console.log(`on port ${port}...`));
