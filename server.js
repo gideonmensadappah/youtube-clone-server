d740aba776df3d3630062c9277f6f150b284920a
Gideon Mensa <gideon123ewq@gmail.com>
2020-03-23 04:21:21 +0200

    initial commit

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
+
 const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(cors());
@@ -11,11 +12,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 const Users = require("./routes/Users");
 const Videos = require("./routes/Videos");
 const Comments = require("./routes/comments");
+
 app.use("/users", Users);
 app.use("/videos", Videos);
 app.use("/comments", Comments);
+
 app.get("/", (req, res) => {
   res.send("start");
 });
-
 app.listen(port, () => console.log(`on port ${port}...`));
